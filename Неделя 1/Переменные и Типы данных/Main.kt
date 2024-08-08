19cd58080567b75c88ec7880992342e58ddd9214
hyphentae <hyphentae@proton.me>
2024-08-08 14:52:37 +0500

    Переменные и Типы данных + Refactoring

---
+++ Неделя 1/Переменные и Типы данных/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val firstName: String = "Miras"
+    val surName: String = "Zhumazhan"
+    println("$firstName $surName")
+
+    var a: Int = 32
+    var b: Int = 4
+    var sum = a + b
+    println("a: $a, b: $b, sum: $sum")
+
+    val name = "Алихан, Мақсат, Мұхаммед, Алина"
+    println(name)
+
+    var ageAlihan: Int = 4
+    var ageMaksat: Int = 7
+    var ageMuhammed: Int = 12
+    var ageAlina: Int = 15
+    println("Возраст Алихана: $ageAlihan, Мақсата: $ageMaksat, Мұхаммеда: $ageMuhammed, Алины: $ageAlina")
+
+    val currentBookPageCount: Int = 592
+    var pageProgress: Int = 12
+    println("Количество страниц в книге: $currentBookPageCount, Прочитанно страниц: $pageProgress")
+
+    val augustDayCount: Int = 31
+    println("День: $augustDayCount")
+
+    val myHomeHeight: Float = 3.8F
+    println("Высота в метрах: $myHomeHeight")
+
+    val pi: Double = 3.141592653589793
+    println("π равен: $pi")
+
+    val any: Any = "https://open.spotify.com/track/6cEI7u5eOOQTmoOtSBwTp8?si=6675d0872a4a4d7c"
+    print(any)
+}
