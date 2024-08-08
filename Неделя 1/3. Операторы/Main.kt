1189557b3300560dafac2f18cd8b78830b0dbcef
hyphentae <hyphentae@proton.me>
2024-08-08 15:24:02 +0500

    Операторы + Refactoring

---
+++ Неделя 1/3. Операторы/Main.kt
@@ -0,0 +1,52 @@
+fun main() {
+    val appleCount = 10
+    val appleFriendCount = 2
+    val applesForEachFriend = appleCount / appleFriendCount
+    println("Количество яблок на каждого друга: $applesForEachFriend")
+
+    val bookCount = 3
+    val eachBookPrice = 900
+    val totalBookPrice = 3 * 900
+    println("Общая стоимость книг: $totalBookPrice")
+
+    val candyCount = 8
+    val candyFriendCount = 4
+    val candiesForEachFriend = candyCount / candyFriendCount
+    println("Каждый друг получит $candiesForEachFriend конфет")
+
+    val workHoursPerDay = 8
+    val workingDaysInWeek = 5
+    val totalWorkHours = workHoursPerDay * workingDaysInWeek
+    println("Я буду работать $totalWorkHours час(ов)")
+
+    val moneyAvailable = 20
+    val booksWanted = 4
+    val eachAvailableBookPrice = 5
+    val totalMoneyLeft = moneyAvailable - (booksWanted * eachAvailableBookPrice)
+    println("У меня останется $totalMoneyLeft долларов")
+
+    val speed = 60
+    val distance = 120
+    val time = distance / speed
+    println("Дорога займет $time час(ов)")
+
+    val tasksPerHour = 5
+    val hours = 8
+    val tasksDone = tasksPerHour * hours
+    println("Я смогу выполнить $tasksDone задач")
+
+    val applesBefore = 15
+    val applesForFriend = 5
+    val applesLeft = applesBefore - applesForFriend
+    println("У меня останется $applesLeft яблок")
+
+    val walkDistance = 3
+    val walkSpeed = 1
+    val walkTime = walkDistance / walkSpeed
+    println("Понадобиться $walkTime час(ов) для достижения цели")
+
+    val chocolateCandyCount = 12
+    val friendCount = 3
+    val eachFriendWillGet = chocolateCandyCount / friendCount
+    println("Каждый друг получит $eachFriendWillGet конфет")
+}
